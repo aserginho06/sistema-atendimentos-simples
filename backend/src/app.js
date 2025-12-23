adc7c9e78122e85c67308b76c9aa12212fce1572
aserginho06 <aserginho06@gmail.com>
2025-12-23 00:07:33 -0300

    fix: corrigir feedback visual do login

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,13 @@ const routes = require("./routes");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 app.use("/api", routes);
 
